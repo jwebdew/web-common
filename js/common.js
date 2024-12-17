7d44086dab6a1c5dbc225ddb75da61ac2c28c8c3
jwebdew <hwebdew@gmail.com>
2024-12-17 15:09:44 +0900

    경로변경

--- js/common.js
+++ js/common.js
@@ -2,7 +2,7 @@ $(function(){
        //푸터추가               
     //load => 서버에 있는 데이터를 가져옴 
 
-        $('#header').load('../common/header.html', function(){
+        $('#header').load('common/header.html', function(){
             $('button').click(function(){
                 $('.nav').toggle()
             });
